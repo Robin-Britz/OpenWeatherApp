77cb1d3dcac01f1ae16a1f301eec45e086054cef
Robin-Britz <36510262+Robin-Britz@users.noreply.github.com>
2024-09-18 21:00:14 +0200

    Add city get endpoint
    get request that takes city name and returns longitude and latitude data for given city from openweather api.

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import typing 
 import fastapi
+import dotenv
+import os
+import requests
+
+dotenv.load_dotenv()
+api_key = os.getenv("API_KEY")
 
 app = fastapi.FastAPI()
-
-with open("creds.env", "r") as key:
-    api_key = key.readline()
-
-# print(api_key)
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"OpenWeather": "API"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: typing.Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/city/")
+def get_city(city: str = fastapi.Query(description="City name")):
+    """Retrieve latitude and longitude for given city."""
+    open_weather_api = f"http://api.openweathermap.org/geo/1.0/direct?q={city}=&appid={api_key}"
+    city_results = requests.get(open_weather_api).json()
+    if city_results:
+        latitude = city_results[0]["lat"]
+        longitude = city_results[0]["lon"]
+        return {"Latitude": latitude, "Longitude": longitude}
+    return {"Error": "No results found"}
+
