d0fd5eaa9760e0312d8cd07f7c35f1f38d54a1b2
Robin-Britz <36510262+Robin-Britz@users.noreply.github.com>
2024-09-19 09:31:52 +0200

    Can now get weather data by longitude and latitude, uses local cached data if not too old.

--- main.py
+++ main.py
@@ -1,28 +1,66 @@
-import typing 
 import fastapi
 import dotenv
 import os
 import requests
+import json
+import datetime
 
 dotenv.load_dotenv()
 api_key = os.getenv("API_KEY")
 
+date_format = "%Y-%m-%d %H:%M:%S.%f"
+
 app = fastapi.FastAPI()
+
+
+def cached_data():
+    if os.path.isfile("weather_data.json"):
+        with open("weather_data.json", "r") as weather_file:
+            timestamp = json.load(weather_file)["Timestamp"]
+            timestamp = datetime.datetime.strptime(timestamp, date_format)
+            difference = datetime.datetime.now() - timestamp
+
+            # 10800 seconds == 3 hours
+            if difference.total_seconds() < 10800:
+                return True
+    return False
 
 
 @app.get("/")
 def read_root():
-    return {"OpenWeather": "API"}
+    return {"Python Fun": "OpenWeather API"}
 
 
 @app.get("/city/")
 def get_city(city: str = fastapi.Query(description="City name")):
     """Retrieve latitude and longitude for given city."""
-    open_weather_api = f"http://api.openweathermap.org/geo/1.0/direct?q={city}=&appid={api_key}"
-    city_results = requests.get(open_weather_api).json()
-    if city_results:
-        latitude = city_results[0]["lat"]
-        longitude = city_results[0]["lon"]
+    geocode_api = f"http://api.openweathermap.org/geo/1.0/direct?q={city}=&appid={api_key}"
+    geocode_api_results = requests.get(geocode_api).json()
+    if geocode_api_results:
+        latitude = geocode_api_results[0]["lat"]
+        longitude = geocode_api_results[0]["lon"]
         return {"Latitude": latitude, "Longitude": longitude}
     return {"Error": "No results found"}
 
+
+@app.get("/weather-by-city/")
+def weather_by_city(latitude: float, longitude: float):
+    """Retrieve weather data for city based on latitude and longitude"""
+    if cached_data():
+        with open("weather_data.json", "r") as weather_file:
+            return json.load(weather_file)
+
+    else:
+        weather_api = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&units=metric&appid={api_key}"
+        weather_data = requests.get(weather_api).json()
+        timestamp = datetime.datetime.now()
+        weather_data["Timestamp"] = str(timestamp)
+
+        with open("weather_data.json", "w") as weather_file:
+            json.dump(weather_data, weather_file)
+
+        with open("weather_data.json", "r") as weather_file:
+            return json.load(weather_file)
+
+
+weather_by_city(-23.0379481, 30.6493787)
